096477c1c668deede2edeb689afd38c55e007497
jinsewoon <jinseun@gmail.com>
2020-12-11 11:23:12 +0900

    FILE_UPLOAD

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+  /*script 영역 table scroll 생성 시 스크롤바 수정 예*/
+  // '.bind() == .live() == .on()' 버전별로 상이
+   // $(window).on("load resize",function(){
+   //   var scrollBarWidth = $(".tbl_content").width() - $(".tbl_content table").width();
+   //   $(".tbl_header").css("padding-right",scrollBarWidth);
+   //   console.log(scrollBarWidth);
+   // }).resize();
+});// document ready
